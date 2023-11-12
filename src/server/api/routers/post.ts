0ec1e852ea66080c4d672e02f9a72adcd121b420
DavidMiller2001 <Davidwaynemiller2001@gmail.com>
2023-11-12 18:45:17 -0500

    access data from db

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -15,19 +15,24 @@ export const postRouter = createTRPCRouter({
       };
     }),
 
-  create: protectedProcedure
-    .input(z.object({ name: z.string().min(1) }))
-    .mutation(async ({ ctx, input }) => {
-      // simulate a slow db call
-      await new Promise((resolve) => setTimeout(resolve, 1000));
+  getAll: publicProcedure.query(async ({ ctx }) => {
+    const data = await ctx.db.post.findMany();
+    return data;
+  }),
 
-      return ctx.db.post.create({
-        data: {
-          name: input.name,
-          createdBy: { connect: { id: ctx.session.user.id } },
-        },
-      });
-    }),
+  // create: protectedProcedure
+  //   .input(z.object({ name: z.string().min(1) }))
+  //   .mutation(async ({ ctx, input }) => {
+  //     // simulate a slow db call
+  //     await new Promise((resolve) => setTimeout(resolve, 1000));
+
+  //     return ctx.db.post.create({
+  //       data: {
+  //         name: input.name,
+  //         createdBy: { connect: { id: ctx.session.user.id } },
+  //       },
+  //     });
+  //   }),
 
   getLatest: protectedProcedure.query(({ ctx }) => {
     return ctx.db.post.findFirst({
