fc2b766ee20b1dde0300865bf9d7bb7204660205
DavidMiller2001 <Davidwaynemiller2001@gmail.com>
2023-11-18 12:43:09 -0500

    style user page

--- src/pages/user/[...id].tsx
+++ src/pages/user/[...id].tsx
@@ -13,9 +13,14 @@ const User = () => {
   if (!user) return <div>Sorry :/</div>;
 
   return (
-    <main className="mx-auto w-full max-w-screen-sm border-x border-black bg-gray-100">
-      <Link href="/">Back</Link>
-      <h1 className="text-center text-xl font-bold">{user?.name}</h1>
+    <main className="mx-auto w-full max-w-screen-sm border-x border-slate-600 text-slate-400">
+      <h1 className="flex border-b border-slate-600 p-4">
+        <Link href="/">Back</Link>
+        <span className="grow text-center text-xl font-bold text-slate-200">
+          {user?.name}
+        </span>
+        <span className="text-transparent">test</span>
+      </h1>
       <ul>
         {posts?.map((post) => {
           return <PostView key={post.id} post={{ post: post, author: user }} />;
